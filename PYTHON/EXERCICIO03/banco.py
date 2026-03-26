1eb0eccedc44170584c4a2ff6371f7bb4ddd89a8
rafaelwebber <rafael.webber09@gmail.com>
2026-03-26 11:23:18 -0300

    feat: mais exercicios de herança

--- PYTHON/EXERCICIO03/banco.py
+++ PYTHON/EXERCICIO03/banco.py
@@ -3,3 +3,8 @@ A importância da herança está relacionada à capacidade de criar novas classe
 Com base no que vimos nessa aula sobre herança, crie uma classe Banco com dois atributos: nome e endereco. Em seguida, derive uma classe chamada 
 Agencia que herda os atributos da classe Banco e inclua um atributo adicional chamado numero. Ambas as classes devem ter apenas o construtor.'''
 
+class Banco:
+    def __init__(self, nome, endereco):
+        self._nome = nome
+        self._endereco = endereco
+        
--- PYTHON/RESTAURANTE/modelos/cardapio/bebida.py
+++ PYTHON/RESTAURANTE/modelos/cardapio/bebida.py
@@ -1,5 +1,7 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
+
+
 class Bebida(ItemCardapio):
     def __init__(self, nome, preco, tamanho):
         super().__init__(nome, preco)
