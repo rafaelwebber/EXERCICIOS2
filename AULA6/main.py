e22bde8c6316fdd2d634e70be38b8731556150a3
rafaelwebber <rafael.webber09@gmail.com>
2026-05-26 20:44:33 -0300

    feat: nova rota

--- AULA6/main.py
+++ AULA6/main.py
@@ -25,6 +25,14 @@ def create_student(student: schemas.EstudanteCreate, db: Session = Depends(get_d
     db.refresh(db_student)
     return db_student
 
+@app.get('/estudante/{estudante_id}', response_model = schemas.EstudanteResponse)
+def ler_estudante(estudante_id: int, db: Session = Depends(get_db)):
+    estudante = db.query(models.Estudante).filter(models.Estudante.id == estudante_id).first()
+    if estudante is None:
+        raise HTTPException(status_code=404, detail="Estudante não encontrado")
+    return estudante
+
+
 @app.get('/estudantes/', response_model = List[schemas.EstudanteResponse])
 def read_students(db:Session = Depends(get_db)):
     students = db.query(models.Estudante).all()
