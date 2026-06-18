772ca94bbe928e9618983cc6fd55d027c4a8d0bd
rafaelwebber <rafael.webber09@gmail.com>
2026-06-18 16:39:33 -0300

    conexão com o banco

--- styles_flask/app/__init__.py
+++ styles_flask/app/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
-from .routes.main import main_bp
-from .routes.category_routes import category_bp
+
+from pymongo import MongoClient
+
+db = None
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config.Config')
+    global db
+
+    try:
+        client = MongoClient(app.config['MONGO_URI'])
+        db = client.get_default_database()
+    except Exception as e:
+        print(f'Erro ao realizar a conexão com o banco de dados: {e}')
+
+
+    from .routes.main import main_bp
+    from .routes.category_routes import category_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(category_bp)
     return app
--- styles_flask/app/routes/main.py
+++ styles_flask/app/routes/main.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from app.models.user import LoginPayload
 main_bp = Blueprint('main_bp', __name__)
 from pydantic import ValidationError
+from app import db
+from bson import ObjectId
 
 
 
@@ -27,9 +29,14 @@ def login():
     return jsonify({"mensagem": f"Realizar login do usuario {user_data.model_dump_json}"})
 
 #RF: O sistema deve permitir a listagem de todos os produtos
-@main_bp.route('/products')
+@main_bp.route('/products', methods=['GET'])
 def get_products():
-    return jsonify({"mensagem":"Esta é a rota de listagem dos produtos"})
+    products_cursor = db.products.find({})
+    products_list = []
+    for products in products_cursor:
+        products['_id'] = str(products['_id'])
+        products_list.append(products)
+    return jsonify(products_list)
 
 
 #RF: O sistema deve permitir a criaação de um novo produto
@@ -38,9 +45,21 @@ def create_product():
     return jsonify({"mensagem":"Esta é a rota de criação de produto"})
 
 #RF: O sistema deve permitir a visualização dos detalhes de um unico produto
-@main_bp.route('/product/<int:product_id>', methods=['GET'])
+@main_bp.route('/products/<string:product_id>', methods=['GET'])
 def get_product_by_id(product_id):
-    return jsonify({"mensagem":f"Esta é a rota de visualização do detalhe do id do produto {product_id}"})
+    try:
+        oid = ObjectId(product_id)
+    except Exception as e:
+        return jsonify({"error":f"erro ao buscar o produto {product_id}: {e}"})
+
+    product = db.products.find_one({'_id':oid})
+
+    if product:
+        product['_id'] = str(product['_id'])
+        return jsonify(product)
+
+    else:
+        return jsonify({"error":f"Produto com o id: {product_id} - Não encontrado"})
 
 #RF: O sistema deve permitir a atualização de um unico produto e prduto existente
 @main_bp.route('/product/<int:product_id>', methods=['PUT'])
