b97d26a37090d3727a7bbda81fbffde81fa966c4
rafaelwebber <rafael.webber09@gmail.com>
2026-06-19 14:59:44 -0300

    token y criação de product

--- styles_flask/app/routes/main.py
+++ styles_flask/app/routes/main.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, current_app, jsonify, request
 from app.models.user import LoginPayload
 main_bp = Blueprint('main_bp', __name__)
 from pydantic import ValidationError
 from app import db
 from bson import ObjectId
 from app.models.products import *
+from app.decorators import token_required
+from datetime import datetime, timedelta, timezone
+import jwt
 
 
 
@@ -18,32 +21,50 @@ def login():
     
     except ValidationError as e:
         return jsonify({"error": e.errors()}), 400
-    except Exception as e:
+    except Exception:
         jsonify({"error": "Erro durante a requisição do dado"}), 500
 
     if user_data.username == 'admin' and user_data.password == '123':
-        return jsonify({"mensage": "Login bem sucedido!"})
+        token = jwt.encode(
+            {
+                "user_id": user_data.username,
+                "exp": datetime.now(timezone.utc) + timedelta(minutes=30)
+            },
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256'
+        )
 
-    else:
-        return jsonify({"mensage": "Credenciais Invalidas!"})
+        return jsonify({'access_token': token}), 200
+
+    return jsonify({"mensage": "Credenciais Invalidas!"}), 401
     
-    return jsonify({"mensagem": f"Realizar login do usuario {user_data.model_dump_json}"})
+
 
 #RF: O sistema deve permitir a listagem de todos os produtos
 @main_bp.route('/products', methods=['GET'])
 def get_products():
     products_cursor = db.products.find({})
     products_list = [ProductDBMondel(**product).model_dump(by_alias=True, exclude_none=True) for product in products_cursor]
-    for products in products_cursor:
-        products['_id'] = str(products['_id'])
-        products_list.append(products)
     return jsonify(products_list)
 
 
 #RF: O sistema deve permitir a criaação de um novo produto
 @main_bp.route('/products', methods=['POST'])
-def create_product():
-    return jsonify({"mensagem":"Esta é a rota de criação de produto"})
+@token_required
+def create_product(token):
+    try:
+        payload = request.get_json(silent=True)
+        if not payload:
+            return jsonify({"error": "Corpo da requisição JSON é obrigatório"}), 400
+
+        product = Product(**payload)
+
+    except ValidationError as e:
+        return jsonify({"error": e.errors()}), 400
+
+    result = db.products.insert_one(product.model_dump())
+    return jsonify({"mensagem":"Esta é a rota de criação de produto",
+                "id": str(result.inserted_id)}), 201
 
 #RF: O sistema deve permitir a visualização dos detalhes de um unico produto
 @main_bp.route('/products/<string:product_id>', methods=['GET'])
--- styles_flask/config.py
+++ styles_flask/config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 class Config:
     MONGO_URI=os.getenv('MONGO_URI')
+    SECRET_KEY=os.getenv('SECRET_KEY')
