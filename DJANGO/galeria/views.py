de4a60bdb292a58cb7a2f0b4aad3f02de8c8d690
rafaelwebber <rafael.webber09@gmail.com>
2026-06-25 15:35:28 -0300

    continuação do projeto com aproveitamento de codigo

--- DJANGO/galeria/views.py
+++ DJANGO/galeria/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {"nome": "Nebulosa de Carina", 
+    "legenda": "webbtelecope.org / NASA / James Webb"},
+    2: {"nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"}
+
+}
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
