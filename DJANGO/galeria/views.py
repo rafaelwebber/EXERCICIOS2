e88649e8100d1f67e15f5b764be806b70492bb6c
rafaelwebber <rafael.webber09@gmail.com>
2026-06-26 09:40:50 -0300

    buscando informações diretamente do banco

--- DJANGO/galeria/views.py
+++ DJANGO/galeria/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-    1: {"nome": "Nebulosa de Carina", 
-    "legenda": "webbtelecope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-}
-    return render(request, 'galeria/index.html', {"cards": dados})
-
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
