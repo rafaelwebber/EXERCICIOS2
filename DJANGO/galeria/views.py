97465350e04cfd2b8a2b4b0d86082e4120a1e557
rafaelwebber <rafael.webber09@gmail.com>
2026-08-12 14:51:12 -0300

    melhoria nas validações de cadastro e login e tratamento de erros

--- DJANGO/galeria/views.py
+++ DJANGO/galeria/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from galeria.models import Fotografia
-
+from django.contrib import messages
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect('login')
+
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
@@ -10,6 +14,10 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect('login')
+
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
