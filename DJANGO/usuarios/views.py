895950cd8b0773414ed55887fcddf1f79ab81e0f
rafaelwebber <rafael.webber09@gmail.com>
2026-08-12 11:51:26 -0300

    fix: melhoria

--- DJANGO/usuarios/views.py
+++ DJANGO/usuarios/views.py
@@ -5,6 +5,9 @@ from usuarios.forms import CadastroForms, LoginForms
 from django.contrib import auth
 
 from django.contrib.auth.models import User
+
+from django.contrib import messages
+
 def login(request):
     form = LoginForms()
 
@@ -22,8 +25,10 @@ def login(request):
             )
             if usuario is not None:
                 auth.login(request, usuario)
+                messages.success(request, f"{nome} logado com sucesso")
                 return redirect ('index')
             else:
+                messages.error(request, "Erro ao efetuar login")
                 return redirect('login')
 
 
@@ -38,6 +43,7 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, "Senhas não são iguais")
                 return redirect('cadastro')
 
             nome=form["nome_cadastro"].value()
@@ -45,6 +51,7 @@ def cadastro(request):
             senha=form["senha_1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Usuario já existe")
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -53,7 +60,14 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, "Cadastro efetuado com sucesso")
             return redirect('login')
 
 
     return render(request, "usuarios/cadastro.html", {"form": form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso! ")
+    return redirect('login')
